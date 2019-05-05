BurntSushi/imdb-rename

commit 09e07a22e9d8eae9b8ad79ecfe347a1d44ac2c0f

data: sort data files after downloading them
The index construction process fairly deeply assumes that we visit
records in the IMDb data in lexicographic order of the `tt` identifiers.
Previously, the data was pre-sorted in this fashion. Unfortunately, the
addition of 10 character identifiers appears to break this property, or
more precisely, `tt1234567`, for example, would appear *after*
`tt12345670`, even though it is lexicographically smaller.

To fix this, we read all CSV data into memory when initially downloading
it, and then sort the data by the first two columns, since some data files
have identifiers in the second column as well. This is regrettable, but
is a simple fix.

Cargo.toml
@@ -17,6 +17,7 @@ members = ["imdb-eval", "imdb-index"]
 
 [dependencies]
 clap = "2.32.0"
+csv = "1"
 failure = "0.1"
 flate2 = "1"
 imdb-index = { version = "0.1.1", path = "imdb-index" }

src/download.rs
@@ -2,6 +2,7 @@ use std::fs::{self, File};
 use std::io;
 use std::path::{Path, PathBuf};
 
+use csv;
 use flate2::read::GzDecoder;
 use reqwest;
 
@@ -60,7 +61,7 @@ fn download_one(outdir: &Path, dataset: &'static str) -> Result<()> {
     let url = format!("{}/{}", IMDB_BASE_URL, dataset);
     info!("downloading {} to {}", url, outpath.display());
     let mut resp = GzDecoder::new(reqwest::get(&url)?.error_for_status()?);
-    io::copy(&mut resp, &mut outfile)?;
+    write_sorted_csv_records(&mut resp, &mut outfile)?;
     Ok(())
 }
 
@@ -84,4 +85,42 @@ fn dataset_path(dir: &Path, name: &'static str) -> PathBuf {
     // We drop the gz extension since we decompress before writing to disk.
     path.set_extension("");
     path
+}
+
+/// Read all CSV data into memory and sort the records in lexicographic order.
+///
+/// This is unfortunately necessary because the IMDb data is no longer sorted
+/// in lexicographic order with respect to the `tt` identifiers. This appears
+/// to be fallout as a result of adding 10 character identifiers (previously,
+/// only 9 character identifiers were used).
+fn write_sorted_csv_records<R: io::Read, W: io::Write>(
+    rdr: R,
+    wtr: W,
+) -> Result<()> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .delimiter(b'\t')
+        .quoting(false)
+        .from_reader(rdr);
+    let mut wtr = csv::WriterBuilder::new()
+        .delimiter(b'\t')
+        .quote_style(csv::QuoteStyle::Never)
+        .from_writer(wtr);
+    wtr.write_byte_record(rdr.byte_headers()?)?;
+
+    let mut records = rdr
+        .byte_records()
+        .map(|r| r.map_err(From::from))
+        .collect::<Result<Vec<csv::ByteRecord>>>()?;
+    // This is a complete hack. Most IMDb data files put the identifier in the
+    // first column. Some data files, however, have identifiers in multiple
+    // columns (e.g., title.episode.tsv), but in those cases, they are always
+    // the first two columns. So we sort by both columns, which is harmless
+    // when the second column is not an identifier.
+    records.sort_by(|r1, r2| (&r1[0], &r1[1]).cmp(&(&r2[0], &r2[1])));
+    for record in records {
+        wtr.write_byte_record(&record)?;
+    }
+    wtr.flush()?;
+    Ok(())
 }

src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate clap;
+extern crate csv;
 #[macro_use]
 extern crate failure;
 extern crate flate2;

commit 2ea88a08549de1ac3b6c65f41eee2578c4569d61

data: sort data without parsing CSV
This fixes a regression introduced in commit 09e07a22, where memory
usage increased quite substantially. Namely, we were reading all of the
CSV records into memory, sorting them and then writing them back out.
This commit changes this process to simply read all *lines* into memory,
sort them and then writing them back out. It turns out that this uses a
lot less memory since CSV records apparently have quite a bit of
overhead. (We might consider looking into that and fixing it, but that's
beyond the scope here.)

Thankfully, this is only correct because the CSV data from IMDb never
has records that span multiple lines. If it did, then this
transformation would produce corrupt output.

Fixes #12

Cargo.toml
@@ -16,8 +16,8 @@ license = "Unlicense/MIT"
 members = ["imdb-eval", "imdb-index"]
 
 [dependencies]
+bstr = { version = "0.1.2", default-features = false, features = ["std"] }
 clap = "2.32.0"
-csv = "1"
 failure = "0.1"
 flate2 = "1"
 imdb-index = { version = "0.1.1", path = "imdb-index" }

src/download.rs
@@ -2,7 +2,6 @@ use std::fs::{self, File};
 use std::io;
 use std::path::{Path, PathBuf};
 
-use csv;
 use flate2::read::GzDecoder;
 use reqwest;
 
@@ -61,6 +60,7 @@ fn download_one(outdir: &Path, dataset: &'static str) -> Result<()> {
     let url = format!("{}/{}", IMDB_BASE_URL, dataset);
     info!("downloading {} to {}", url, outpath.display());
     let mut resp = GzDecoder::new(reqwest::get(&url)?.error_for_status()?);
+    info!("sorting CSV records");
     write_sorted_csv_records(&mut resp, &mut outfile)?;
     Ok(())
 }
@@ -97,29 +97,25 @@ fn write_sorted_csv_records<R: io::Read, W: io::Write>(
     rdr: R,
     wtr: W,
 ) -> Result<()> {
-    let mut rdr = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .delimiter(b'\t')
-        .quoting(false)
-        .from_reader(rdr);
-    let mut wtr = csv::WriterBuilder::new()
-        .delimiter(b'\t')
-        .quote_style(csv::QuoteStyle::Never)
-        .from_writer(wtr);
-    wtr.write_byte_record(rdr.byte_headers()?)?;
+    use std::io::Write;
+    use bstr::io::BufReadExt;
 
-    let mut records = rdr
-        .byte_records()
-        .map(|r| r.map_err(From::from))
-        .collect::<Result<Vec<csv::ByteRecord>>>()?;
-    // This is a complete hack. Most IMDb data files put the identifier in the
-    // first column. Some data files, however, have identifiers in multiple
-    // columns (e.g., title.episode.tsv), but in those cases, they are always
-    // the first two columns. So we sort by both columns, which is harmless
-    // when the second column is not an identifier.
-    records.sort_by(|r1, r2| (&r1[0], &r1[1]).cmp(&(&r2[0], &r2[1])));
-    for record in records {
-        wtr.write_byte_record(&record)?;
+    // We actually only sort the raw lines here instead of parsing CSV records,
+    // since parsing into CSV records has fairly substantial memory overhead.
+    // Since IMDb CSV data never contains a record that spans multiple lines,
+    // this transformation is okay.
+    let rdr = io::BufReader::new(rdr);
+    let mut lines = rdr.byte_lines().collect::<io::Result<Vec<_>>>()?;
+    if lines.is_empty() {
+        bail!("got empty CSV input");
+    }
+    // Keep the header record first.
+    lines[1..].sort_unstable();
+
+    let mut wtr = io::BufWriter::new(wtr);
+    for line in lines {
+        wtr.write_all(line.as_bytes())?;
+        wtr.write_all(b"\n")?;
     }
     wtr.flush()?;
     Ok(())

src/main.rs
@@ -1,6 +1,6 @@
+extern crate bstr;
 #[macro_use]
 extern crate clap;
-extern crate csv;
 #[macro_use]
 extern crate failure;
 extern crate flate2;